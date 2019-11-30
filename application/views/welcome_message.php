c7df7adcbe6de1bd96418aadcb37062c90c12664
IbraHarsye <muhammadharsyeibra1410@gmail.com>
2019-11-30 14:21:24 +0700

    First Commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+            <!-- Navbar -->
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <a class="navbar-brand" href="#">My Portofolio</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                  
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Education</a>
+                        </li>
+                        <li class="nav-item">
+                                <a class="nav-link" href="#">Experience</a>
+                        </li>
+                        <li class="nav-item">
+                                <a class="nav-link" href="#">Skills</a>
+                        </li>
+                      </ul>
+                      <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-warning my-2 my-sm-0" type="submit">Search</button>
+                      </form>
+                    </div>
+                  </nav>
+            <!-- Akhir Navbar -->
+           <!-- Jumbotron -->
+    <div class="jumbotron jumbotron-fluid text-center">
+        <div class="container">
+            <img src="<?php echo base_url('assets/background.png')?>" alt="" class="w-25 rounded-circle"> 
+          <h1 class="display-4">Muhammad Harsye Ibra</h1>
+          <p class="lead">Belajar Web dengan Bootstrap 4</p>
+        </div>
+    </div>
+
+    <!-- Akhir Jumbotron -->
+
+    <!-- Grid -->
+    <div class="container text-center">
+            <div class="row">
+              <div class="col-sm">
+                <h2>About</h2>
+                <p class="text-justifyx">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium maxime aliquam, illo numquam atque nobis quam ad nesciunt fugiat sint at aspernatur explicabo voluptatum distinctio autem dolor accusantium! Dignissimos, enim?</p>
+              </div>
+              <div class="col-sm">
+                <h2>Experience</h2>
+                <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores odio numquam, exercitationem fuga rerum at, deleniti placeat vitae illum earum natus fugiat, eius doloremque! Nobis rerum sapiente sequi ducimus repellendus.</p>
+              </div>
+            </div>
+          </div>
+  </div>
+    <!-- AKhir Grid -->
+
+    <!-- Footer -->
+    <div class="footer bg-dark fixed-bottom">
+        <p class="text-center text-light"> &copy; Muhammad Harsye Ibra 2019</p>
+    </div>
+    <!-- AKhir Footer -->
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
